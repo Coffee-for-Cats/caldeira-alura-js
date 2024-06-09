4a63e2ce4d4ee4a36081c28bae983bcf182dfb0d
Lucas Schwalm Silva <lnaoedelixo42@gmail.com>
2024-06-09 14:57:56 -0300

    Alugar games feito!

---
+++ alugames/js/app.js
@@ -0,0 +1,17 @@
+const games = document.getElementsByClassName('dashboard__items__item');
+
+function alterarStatus(id) {
+  const game = games[id - 1];
+  const button = game.querySelector('.dashboard__item__button');
+  // console.log('button: ', button)
+  const image = game.querySelector('.dashboard__item__img');
+  // console.log('image: ', image)
+
+  if (button.classList.contains('dashboard__item__button--return')) {
+    button.classList.remove('dashboard__item__button--return');
+    image.classList.remove('dashboard__item__img--rented');
+  } else {
+    button.classList.add('dashboard__item__button--return');
+    image.classList.add('dashboard__item__img--rented');
+  }
+}
